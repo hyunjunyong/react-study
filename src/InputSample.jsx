9c863b5f12beee53072913453fb0c112317a8126
현준용 <hjy4649@cocen.com>
2023-03-15 16:15:00 +0900

    여러개 input

--- src/InputSample.jsx
+++ src/InputSample.jsx
@@ -1,24 +1,41 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
-function InputSample(){
-  const [text, setText] = useState('')
-  const onChange = e=>{
-    setText(e.target.value)
-  }
-  const onClear = e=>{
-    setText('')
-  }
-  return(
+function InputSample() {
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: ''
+  });
+
+  const { name, nickname } = inputs; 
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    
+    setInputs({
+      ...inputs, 
+      [name]: value 
+    });
+  };
+
+  const onReset = () => {
+    setInputs({
+      name: '',
+      nickname: '',
+    })
+  };
+
+
+  return (
     <div>
-      <input type="text" onChange={onChange} value={text}/>
-      <button onClick={onClear
-      }>초기화</button>
+      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
+      <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: {text}</b>
+        <b>값: </b>
+        {name} ({nickname})
       </div>
     </div>
-  )
-  
+  );
 }
 
-export default InputSample
+export default InputSample;
