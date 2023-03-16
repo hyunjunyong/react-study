64a1a53a310e213cf7e6ed456fbe18db49e314de
현준용 <hjy4649@cocen.com>
2023-03-16 13:59:23 +0900

    array rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Counter from './counter'
 import InputSample from './InputSample'
+import UserList from './UserList'
 function App() {
   return (
     <div className="App">
       <Counter />
       <InputSample />
+      <UserList />
     </div>
   );
 }
