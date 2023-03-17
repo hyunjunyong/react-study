9dd64218b337754ed6f87d8689e0cd9999cbeab8
현준용 <hjy4649@cocen.com>
2023-03-17 13:54:38 +0900

    react array 추가제거수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,74 @@
+import React, { useState,useRef } from 'react';
 import './App.css';
 import Counter from './counter'
 import InputSample from './InputSample'
 import UserList from './UserList'
+import CreateUser from './CreateUser'
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email:'',
+  })
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users,setUsers]= useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ])
+  const nextId = useRef(4)
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]);
+    // 아래로 대체가능
+    // setUsers(users.concat(user));
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  };
+  const onRemove = id =>{
+    setUsers(users.filter(user=>user.id !== id))
+  }
+  const onToggle = id =>{
+    setUsers(users.map(user=>user.id === id? {...user, active: !user.active} : user))
+  }
   return (
     <div className="App">
       <Counter />
       <InputSample />
-      <UserList />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <CreateUser  
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+        />
+        
     </div>
   );
 }
