dafc2117beb6b152751942801039b5eac6cd765a
현준용 <hjy4649@cocen.com>
2023-03-20 18:01:08 +0900

    useEffect useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import React, { useState,useRef } from 'react';
+import React, { useState,useRef,useMemo,useCallback } from 'react';
 import './App.css';
 import Counter from './counter'
 import InputSample from './InputSample'
 import UserList from './UserList'
 import CreateUser from './CreateUser'
+
+function countActiveUsers(users){
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user=>user.active).length
+}
+
 function App() {
   const [inputs, setInputs] = useState({
     username: '',
     email:'',
   })
   const { username, email } = inputs;
-  const onChange = e => {
+  const onChange = useCallback(e => {
     const { name, value } = e.target;
     setInputs({
       ...inputs,
       [name]: value
     });
-  };
+  },[inputs]);
   const [users,setUsers]= useState([
     {
       id: 1,
@@ -35,28 +41,40 @@ function App() {
     }
   ])
   const nextId = useRef(4)
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
       email
     };
-    setUsers([...users, user]);
-    // 아래로 대체가능
-    // setUsers(users.concat(user));
+    setUsers(users.concat(user));
 
     setInputs({
       username: '',
       email: ''
     });
     nextId.current += 1;
-  };
-  const onRemove = id =>{
-    setUsers(users.filter(user=>user.id !== id))
-  }
-  const onToggle = id =>{
-    setUsers(users.map(user=>user.id === id? {...user, active: !user.active} : user))
-  }
+  }, [users, username, email]);
+
+  const onRemove = useCallback(
+    id => {
+      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+      // = user.id 가 id 인 것을 제거함
+      setUsers(users.filter(user => user.id !== id));
+    },
+    [users]
+  );
+  const onToggle = useCallback(
+    id => {
+      setUsers(
+        users.map(user =>
+          user.id === id ? { ...user, active: !user.active } : user
+        )
+      );
+    },
+    [users]
+  );
+  const count = useMemo(()=>countActiveUsers(users),[users]);
   return (
     <div className="App">
       <Counter />
@@ -68,7 +86,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
         />
-        
+        <div>활성사용자 수 : {count}</div>
     </div>
   );
 }
