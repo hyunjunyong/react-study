02d4d96be06a801f560b29e755c35c67b80bbd72
현준용 <hjy4649@cocen.com>
2023-03-21 11:08:22 +0900

    use reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
-import React, { useRef, useState, useMemo, useCallback } from 'react';
+import React, { useRef, useState, useMemo, useCallback,useReducer } from 'react';
 import UserList from './component/UserList';
 import CreateUser from './component/CreateUser';
+import Counter from './component/counter';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
   return users.filter(user => user.active).length;
 }
-
-function App() {
-  const [inputs, setInputs] = useState({
+const initialState = {
+  inputs: {
     username: '',
     email: ''
-  });
-  const { username, email } = inputs;
-  const onChange = useCallback(e => {
-    const { name, value } = e.target;
-    setInputs(inputs => ({
-      ...inputs,
-      [name]: value
-    }));
-  }, []);
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
       id: 1,
       username: 'velopert',
@@ -39,37 +31,86 @@ function App() {
       email: 'liz@example.com',
       active: false
     }
-  ]);
+  ]
+};
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      };
+    case 'CREATE_USER':
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      };
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        )
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      };
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    setUsers(users => users.concat(user));
 
-    setInputs({
-      username: '',
-      email: ''
+  const { users } = state;
+  const { username, email } = state.inputs;
+
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    });
+  }, []);
+
+  const onCreate = useCallback(() => {
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
     });
     nextId.current += 1;
   }, [username, email]);
 
-  const onRemove = useCallback(id => {
-    setUsers(users => users.filter(user => user.id !== id));
-  }, []);
   const onToggle = useCallback(id => {
-    setUsers(users =>
-      users.map(user =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+  }, []);
+
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
   }, []);
   const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <div className="App">
+      <Counter />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
       <CreateUser  
         username={username}
--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -1,16 +1,26 @@
-import {useState} from 'react'
+import {useReducer, useState} from 'react'
+
+const reducer = (state, action) =>{
+  switch (action.type){
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
 
 function Counter() {
-  const [number, setNumber] = useState(0);
-  // 첫번째 원소는 현재 상태, 두번째 원소는 Setter 함수
-  const onIncrease = () =>{
-    setNumber(prevNum => prevNum+1); 
-    // setNumber(number+1)
-  }
-  const onDecrease = () =>{
-    setNumber(prevNum => prevNum-1); 
-    // setNumber(number-1)
-  }
+  const [number, dispatch] = useReducer(reducer,0);
+
+  const onIncrease = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
+
+  const onDecrease = () => {
+    dispatch({ type: 'DECREMENT' });
+  };
   return (
     <div >
       <h1>{number}</h1>
