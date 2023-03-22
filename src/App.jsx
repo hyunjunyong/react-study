71425299f2a8b3d26d7457a5e77264724f1f695a
현준용 <hjy4649@cocen.com>
2023-03-22 16:09:07 +0900

    fix(immer): 예시 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useReducer, useMemo } from 'react';
 import UserList from './component/UserList';
 import CreateUser from './component/CreateUser';
-// import produce from 'immer'
+import produce from 'immer'
 
 // 기본 Immer 예제
 // const todo = {
@@ -49,25 +49,24 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case 'CREATE_USER':
-      return {
-        users: state.users.concat(action.user)
-      };
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      });
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user =>
-          user.id === action.id ? { ...user, active: !user.active } : user
-        )
-      };
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      });
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      };
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
     default:
       return state;
   }
 }
+
 
 // UserDispatch 라는 이름으로 내보내줍니다.
 export const UserDispatch = React.createContext(null);
