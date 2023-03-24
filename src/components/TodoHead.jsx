95ee9c44402570c188a4b66f71b7994ba0c43767
현준용 <hjy4649@cocen.com>
2023-03-24 18:07:24 +0900

    context api

--- src/components/TodoHead.jsx
+++ src/components/TodoHead.jsx
@@ -1,9 +1,13 @@
 //  오늘의 날짜 요일 , 해야 할일
 import React from 'react'
 import styled from 'styled-components'
+import { useTodoState } from '../TodoContext'
 
 const TodoHeadBlock = styled.div`
-  padding: 48px 32px 24px;
+  padding-top: 48px;
+  padding-left: 32px;
+  padding-right: 32px;
+  padding-bottom: 24px;
   border-bottom: 1px solid #e9ecef;
   h1 {
     margin: 0;
@@ -23,10 +27,12 @@ const TodoHeadBlock = styled.div`
   }
 `
 
-const TodoHead = () => {
+function TodoHead() {
+  const todos = useTodoState()
+  console.log(todos)
   return (
     <TodoHeadBlock>
-      <h1>2023년 7월 10일</h1>
+      <h1>2019년 7월 10일</h1>
       <div className="day">수요일</div>
       <div className="tasks-left">할 일 2개 남음</div>
     </TodoHeadBlock>
