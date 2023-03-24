7a9533bb0445ed026e87af90031f984391fcb7cb
현준용 <hjy4649@cocen.com>
2023-03-24 14:55:13 +0900

    Head , Templage(todo)

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,22 @@
-import React, { useReducer, useMemo } from 'react'
-import UserList from './component/ex/exUserList'
-import CreateUser from './component/ex/exCreateUser'
-import produce from 'immer'
+import React from 'react'
+import { createGlobalStyle } from 'styled-components'
+import TodoTemplate from '../src/component/TodoTemplate'
+import TodoHead from '../src/component/TodoHead'
 
-// 클래스형 컴포넌트 예제 잘 안쓰임 이제
-// class Hello extends Component {
-//   render() {
-//     const { color, name, isSpecial } = this.props;
-//     return (
-//       <div style={{ color }}>
-//         {isSpecial && <b>*</b>}
-//         안녕하세요 {name}
-//       </div>
-//     );
-//   }
-// }
-
-// Hello.defaultProps = {
-//   name: '이름없음'
-// };
-
-// 기본 Immer 예제
-// const todo = {
-//   text: 'Hello',
-//   done: false
-// };
-
-// const updater = produce(draft => {
-//   draft.done = !draft.done;
-// });
-
-// const nextTodo = updater(todo);
-
-// console.log(nextTodo);
-
-function countActiveUsers(users) {
-  console.log('활성 사용자 수를 세는중...')
-  return users.filter((user) => user.active).length
-}
-
-const initialState = {
-  users: [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-      active: true,
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com',
-      active: false,
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com',
-      active: false,
-    },
-  ],
-}
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'CREATE_USER':
-      return produce(state, (draft) => {
-        draft.users.push(action.user)
-      })
-    case 'TOGGLE_USER':
-      return produce(state, (draft) => {
-        const user = draft.users.find((user) => user.id === action.id)
-        user.active = !user.active
-      })
-    case 'REMOVE_USER':
-      return produce(state, (draft) => {
-        const index = draft.users.findIndex((user) => user.id === action.id)
-        draft.users.splice(index, 1)
-      })
-    default:
-      return state
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
   }
-}
-
-// UserDispatch 라는 이름으로 내보내줍니다.
-export const UserDispatch = React.createContext(null)
+`
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState)
-
-  const { users } = state
-
-  const count = useMemo(() => countActiveUsers(users), [users])
   return (
-    <UserDispatch.Provider value={dispatch}>
-      <CreateUser />
-      <UserList users={users} />
-      <div>활성사용자 수 : {count}</div>
-    </UserDispatch.Provider>
+    <>
+      <GlobalStyle />
+      <TodoTemplate children={'hello'}>
+        <TodoHead />
+      </TodoTemplate>
+    </>
   )
 }
 
